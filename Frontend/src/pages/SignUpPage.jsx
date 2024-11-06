10cb1c59060a2b57f6a13648b3594fb54a7c56c2
Soumik Sen <mastersoumik2003@gmail.com>
2024-11-07 03:42:26 +0530

    Update SignUpPage.jsx

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -54,7 +54,7 @@ function SignupPage() {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_URL}/api/v1/users/register`,
+        `https://predictix-v2.onrender.com/api/v1/users/register`,
         {
           method: "POST",
           body: JSON.stringify({ fullname, username, email, password }),
