652a697384063adb5d489fea4b007a388087abee
Soumik Sen <mastersoumik2003@gmail.com>
2024-11-07 10:38:39 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -124,6 +124,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "None", // Allows cross-origin cookies
     path:"/",
   };
 
